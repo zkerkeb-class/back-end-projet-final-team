e1dd3377947237185bb57108127986093202f2fc
Maksymilian Czadowski <maksymilian.czadowski@arondor.com>
2024-11-27 16:48:15 +0100

    feat: verification is username or email already exist

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,19 @@
 const Express = require('express');
 const AuthController = require('../controllers/auth.controller');
-const { validateUser, validateLogin } = require('../middlewares/user.schema');
+const {
+  validateUser,
+  validateLogin,
+  userAlreadyExists,
+} = require('../middlewares/user.schema');
 
 const router = Express.Router();
 
-router.post('/register', validateUser, AuthController.registerUser);
+router.post(
+  '/register',
+  validateUser,
+  userAlreadyExists,
+  AuthController.registerUser,
+);
 router.post('/login', validateLogin, AuthController.loginUser);
 
 module.exports = router;
